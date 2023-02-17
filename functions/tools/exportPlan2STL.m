d9d0b27524a418e54ae9ef0c9e0c6ca7bf91d197
Lucian Hotoiu <lhotoiu@goiba.net>
2023-02-17 09:52:31 +0100

    add export RS plan to STL function

---
+++ functions/tools/exportPlan2STL.m
@@ -0,0 +1,48 @@
+%% exportPlan2STL
+% Export the CEM inside flash plan to a STL file on disk
+%
+%% Syntax
+% |exportPlan2STL(plan_filename)|
+%
+%
+%% Description
+% |exportPlan2STL(plan_filename)| Function exporting the CEM inside a flashplan to STL file on disk in the same folder as the dicom plan
+%
+%% Input arguments
+%
+% |plan_filename| -_STRING_- location and name of flash plan on disk
+%
+%% Output arguments
+%
+% None
+%
+% TODO STL files do not specify what units their distances are in. When a program opens a STL file,
+% it only knows that the model measures a certain number of units in each dimension. [3]
+% [4] There is no way to convey the units used in an STL file. You do need to specify units when you generate the file,
+% but you also need to know what units were used when you import it, as units are not specified in the file itself.
+% In the 3D printing world, this generally doesn't cause too many problems, as everyone just specifies mm,
+% since this is what most printers understand natively anyhow. STEP files for instance contain the units used in the file,
+% so objects always import correctly, unless you override the units. Alas, this is not the case for STL files.
+%
+%% Contributors
+% Authors : L. Hotoiu (open.reggui@gmail.com)
+
+function exportPlan2STL(plan_filename)
+
+    [plan_filepath, plan_name, plan_ext] = fileparts(plan_filename);
+      
+    handles = Initialize_reggui_handles();
+    handles.dataPath = plan_filepath;
+    
+    Plan.ScannerDirectory = 'default';
+    [handles, Plan] = parseFLASHplan(plan_filename, Plan, handles);
+
+    %Remove voxels all around the base in order to close the STL object
+    Plan.Beams.RangeModulator.CEM3Dmask(1,:,:) = 0; %Remove the resin below the base of the CEF
+    Plan.Beams.RangeModulator.CEM3Dmask(end,:,:) = 0; %Remove the resin below the base of the CEF
+    Plan.Beams.RangeModulator.CEM3Dmask(:,1,:) = 0; %Remove the resin below the base of the CEF
+    Plan.Beams.RangeModulator.CEM3Dmask(:,end,:) = 0; %Remove the resin below the base of the CEF
+    
+    stl_filename = fullfile(plan_filepath, [plan_name '.stl']);
+    exportCEM2STL(Plan.Beams.RangeModulator.CEM3Dmask  , Plan.Beams.RangeModulator.Modulator3DPixelSpacing , Plan.Beams.RangeModulator.ModulatorOrigin , Plan.Beams.RangeModulator.AccessoryCode , stl_filename)
+end
